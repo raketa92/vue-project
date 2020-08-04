725f4094d9b261ba085754ef8e6498753eaff441
raketa92 <nazar.erdem@gmail.com>
2020-08-04 06:12:11 +0500

    init commit

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <mdb-navbar position="top" color="orange" scrolling class="navbar-ligth lighten-5">
+        <mdb-navbar-brand href="https://sourcelink.co.uk" target="_blank">
+            Sourcelink
+        </mdb-navbar-brand>
+        <mdb-navbar-toggler>
+            <mdb-navbar-nav center color="blue-grey">
+                <mdb-nav-item :to="{name: 'home'}" waves-fixed active class="active">
+                    Home
+                </mdb-nav-item>
+                <mdb-nav-item :to="{name: 'about'}" waves-fixed active class="active">
+                    About
+                </mdb-nav-item>
+                <mdb-nav-item :to="{name: 'dashboard'}" waves-fixed active class="active">
+                    Dashboard
+                </mdb-nav-item>
+            </mdb-navbar-nav>
+            <mdb-navbar-nav right color="blue-grey">
+                <mdb-nav-item :to="{name: 'login'}" waves-fixed active class="active">
+                    Login
+                </mdb-nav-item>
+            </mdb-navbar-nav>
+        </mdb-navbar-toggler>
+    </mdb-navbar>
+</template>
+
+<script>
+import {
+    mdbNavbar, mdbNavbarBrand, mdbNavItem, mdbNavbarNav, mdbNavbarToggler
+} from "mdbvue"
+
+export default {
+    name: 'Header',
+    components: {
+        mdbNavbar, mdbNavbarBrand, mdbNavItem, mdbNavbarNav, mdbNavbarToggler
+    }
+}
+</script>
